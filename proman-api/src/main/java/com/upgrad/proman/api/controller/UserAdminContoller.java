4d21a3aca3c17b42c6af7a8fb63bd6c84662fb21
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2020-11-14 07:07:48 +0530

    Added ResourceNotFoundException functionality

--- proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminContoller.java
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminContoller.java
@@ -5,6 +5,7 @@ import com.upgrad.proman.api.model.UserDetailsResponse;
 import com.upgrad.proman.api.model.UserStatusType;
 import com.upgrad.proman.service.business.UserAdminBusinessService;
 import com.upgrad.proman.service.entity.UserEntity;
+import com.upgrad.proman.service.exception.ResourceNotFoundException;
 import com.upgrad.proman.service.type.UserStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ public class UserAdminContoller {
     private UserAdminBusinessService userAdminBusinessService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/users/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<UserDetailsResponse> getUser(@PathVariable("id") final String userUuid) {
+    public ResponseEntity<UserDetailsResponse> getUser(@PathVariable("id") final String userUuid) throws ResourceNotFoundException {
         final UserEntity userEntity = userAdminBusinessService.getUser(userUuid);
         UserDetailsResponse userDetailsResponse = new UserDetailsResponse().id(userEntity.getUuid())
                 .firstName(userEntity.getFirstName())
